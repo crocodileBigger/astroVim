9865cd62deb54e39b7a28fbce16cb5a2d12acfa2
bigg <infobighenrique225@gmail.com>
2025-09-10 21:49:29 -0300

    feat: adicionar script que detecta distro, instala snapd e configura nvim

--- nvim_Install.sh
+++ nvim_Install.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
+# Script para identificar a distribuição base e instalar Neovim via snap
 
-sudo apt install snap -y 
+if command -v dnf &>/dev/null; then
+  sudo dnf install -y snapd
+  echo "Base: Red Hat / Fedora (dnf encontrado)"
 
-sudo apt install nvim --classic
+elif command -v yum &>/dev/null; then
+  sudo yum install -y snapd
+  echo "Base: Red Hat / CentOS (yum encontrado)"
 
-mkdir -p ~/.config/
+elif command -v apt &>/dev/null; then
+  sudo apt update && sudo apt install -y snapd
+  echo "Base: Debian / Ubuntu (apt encontrado)"
 
-mv astroVim ~/.config/nvim
+elif command -v zypper &>/dev/null; then
+  sudo zypper install -y snapd
+  echo "Base: SUSE (zypper encontrado)"
+
+elif command -v pacman &>/dev/null; then
+  sudo pacman -Sy --noconfirm snapd
+  echo "Base: Arch Linux (pacman encontrado)"
+
+elif command -v apk &>/dev/null; then
+  sudo apk add snapd
+  echo "Base: Alpine Linux (apk encontrado)"
+
+else
+  echo "Não consegui identificar pelo gerenciador de pacotes."
+fi
+
+echo "Instalando Neovim atualizado..."
+sudo snap install nvim --classic
+
+if [[ ! -d ~/.config ]]; then
+  echo "Criando ~/.config"
+  mkdir -p ~/.config
+fi
+
+if [[ -d ~/.config/nvim ]]; then
+  echo "Salvando configurações antigas (.bak)"
+  mv ~/.config/nvim{,.bak}
+  mv ~/.local/share/nvim{,.bak}
+  mv ~/.local/state/nvim{,.bak}
+  mv ~/.cache/nvim{,.bak}
+fi
+
+echo "Movendo novas configurações do nvim"
+mv nvim ~/.config/nvim
 
 nvim
